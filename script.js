6c56f89a7ece067ff65440c633c5b96ab03d99f7
YounesAEO <y.aboueleomoum09@gmail.com>
2022-08-18 16:23:02 +0200

    implement the map, navigation and direction

---
+++ script.js
@@ -0,0 +1,34 @@
+mapboxgl.accessToken =
+	'pk.eyJ1IjoieW91bmVzYWVvIiwiYSI6ImNsNnowcXdxbjAxbWUzY3FmYzBzOWtkenIifQ.frYy06LMNhyQUbz53p5Okw';
+navigator.geolocation.getCurrentPosition(sucessLocation, errorLocation, {
+	enableHighAccuracy: true,
+});
+
+function sucessLocation(userPos) {
+	console.log(userPos);
+	setupMap([userPos.coords.longitude, userPos.coords.latitude]);
+}
+
+function errorLocation() {
+	setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+	// initializing the map
+	const map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center,
+		zoom: 14,
+	});
+
+	// adding navigation controls
+	const nav = new mapboxgl.NavigationControl();
+	map.addControl(nav);
+
+	// adding direction controls
+	const directions = new MapboxDirections({
+		accessToken: mapboxgl.accessToken,
+	});
+	map.addControl(directions, 'top-left');
+}
